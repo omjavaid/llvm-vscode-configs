c4bdaac87e36e48ec029971cdfd843802d2dc07d
Leandro Lupori <leandro.lupori@linaro.org>
2023-01-24 13:17:55 +0000

    Add flang variant
    Also simplify clang-aarch64-linux.cmake and clang-armhf-linux.cmake.
    Using system's clang, the extra gcc include paths doesn't seem
    necessary (tested with clang-aarch64-linux only).

--- .vscode/clang-aarch64-linux.cmake
+++ .vscode/clang-aarch64-linux.cmake
@@ -1,27 +1,4 @@
-set(TARGET_TRIPLE "aarch64-linux-gnu")
-
-set(CMAKE_C_COMPILER "/home/omair/work/tools/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-16.04/bin/clang")
-
-set(CMAKE_CXX_COMPILER "/home/omair/work/tools/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-16.04/bin/clang++")
-
-set(CMAKE_LIBRARY_ARCHITECTURE "${TARGET_TRIPLE}")
-
-execute_process(
-    COMMAND "gcc" -dumpversion
-    RESULT_VARIABLE HAD_ERROR
-    OUTPUT_VARIABLE GCC_V3
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-set(GCC_INC "/usr/${TARGET_TRIPLE}/include")
-
-set(CMAKE_C_FLAGS "-target ${TARGET_TRIPLE} -I/${GCC_INC} -I/${GCC_INC}/c++/${GCC_V3}/${TARGET_TRIPLE}")
-
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}")
-
-execute_process(
-    COMMAND "gcc" -dumpmachine
-    RESULT_VARIABLE HAD_ERROR
-    OUTPUT_VARIABLE BUILD_ENV_TRIPLE
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-set(CMAKE_IGNORE_PATH "/usr/lib/${BUILD_ENV_TRIPLE}")
+set(LLVM_TARGET_TRIPLE "aarch64-linux-gnu")
+set(CMAKE_C_COMPILER "clang-12")
+set(CMAKE_CXX_COMPILER "clang++-12")
+set(CMAKE_LIBRARY_ARCHITECTURE "${LLVM_TARGET_TRIPLE}")
